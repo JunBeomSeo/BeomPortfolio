12787e4f334bd4592278357adbac923019f5241f
GUI_junbeom <tjwnsqja12@naver.com>
2025-12-23 22:07:46 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -3,10 +3,16 @@ const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height; // getBoundingClientRect() 함수를 이용해서 헤더의 높이를 반환받기
 document.addEventListener("scroll", function () {
   // 스크롤되는 Y 좌표가 headerHeight보다 크다면 다른 스타일링!
-  console.log(window.scrollY);
   if (window.scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함.
+const home = document.querySelector(".home__container");
+const homeheight = home.offsetHeight;
+document.addEventListener("scroll", function () {
+  home.style.opacity = 1 - window.scrollY / homeheight;
 });
