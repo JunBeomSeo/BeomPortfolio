67430fe353052e3281815ebe312b350e7a3848bb
GUI_junbeom <tjwnsqja12@naver.com>
2025-12-28 18:37:07 +0900

    Opserver-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -12,8 +12,12 @@ const navItems = sectionIds.map(
   (id) => document.querySelector(`[href="${id}"]`) //` ` ⭕ → 템플릿 리터럴 (변수 사용 가능)
 );
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -25,16 +29,26 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 && // entry의 index가 가장 마지막이고
       entry.isIntersecting && // 그 entry가 보여지고 있고
-      entry.intersectionRatio >= 0.99; // 99퍼센트가 다 보여진다면
+      entry.intersectionRatio >= 0.95; // 99퍼센트가 다 보여진다면
     // selectLastOne 을 true로 ,,
   });
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
